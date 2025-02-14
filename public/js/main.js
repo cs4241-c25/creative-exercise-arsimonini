a9d5b61d9ee72f3ae6f1de8be57aa286c1a871ce
arsimonini <arsimonini@wpi.edu>
2025-02-14 00:28:21 -0500

    Added various shapes, rotation and gradients

--- public/js/main.js
+++ public/js/main.js
@@ -7,14 +7,25 @@
 
 window.onload = function() {
     drawRect(100, 100, 25, 50, "green")
-    drawLine(250, 210, 260, 180)
+    drawLine(250, 210, 429, 180)
     drawCircle(400, 500, 40, 0, 2)
     advDrawLine(10, 600, 800, 600, 20, "yellow", true)
     advDrawRect(600, 300, 100, 100, "orange", "blue", 7)
 
     advDrawRect(710, 300, 100, 100, "rgb(0 255 0 / 50%)", "rgb(0 0 255 / 50%)", 7)
-}
 
+    drawTriangle(50, 100, 260, 100, 260, 260, "red")
+
+    drawRect(1000, 300, 300, 300, "purple");
+    clearRectangle(1200, 500, 50, 80)
+
+    gradiantRect(600, 100, 400, 100, 600, 200, 1000, 200, "darkblue", "lightblue");
+    gradiantText(400, 75, "red", "purple", "blue", "This is my shape test page")
+
+    gradiantText(750, 780, "orange", "magenta", "green", "Much WoW!")
+
+    rotatedRect(200, 200, 200, 200, 25, "magenta");
+}
 function drawRect(x, y, width, height, color) {
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
@@ -81,3 +92,49 @@ function drawTriangle(startX, startY, secPointX, secPointY, lastPointX, lastPoin
     ctx.stroke();
 }
 
+function clearRectangle(x, y, width, height) {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    ctx.clearRect(x, y, width, height);
+}
+
+function gradiantRect(x,y,width,height,gradStartX, gradStartY, gradEndX, gradEndY, col1, col2) {
+    const c = document.getElementById("myCanvas");
+    const ctx = c.getContext("2d");
+
+// Create linear gradient
+    const grad=ctx.createLinearGradient(gradStartX,gradStartY, gradEndX,gradEndY);
+    grad.addColorStop(0, col1);
+    grad.addColorStop(1, col2);
+
+// Fill rectangle with gradient
+    ctx.fillStyle = grad;
+    ctx.fillRect(x,y, width,height);
+}
+
+function gradiantText(x, y, col1, col2, col3, text) {
+    const c = document.getElementById("myCanvas");
+    const ctx = c.getContext("2d");
+
+// Create linear gradient
+    const grad=ctx.createLinearGradient(x,y,x+500,y+50);
+    grad.addColorStop(0, col1);
+    grad.addColorStop(0.5, col2);
+    grad.addColorStop(1, col3);
+
+// Fill text with gradient
+    ctx.font = "50px Arial";
+    ctx.fillStyle = grad;
+    ctx.fillText(text,x,y);
+}
+
+function rotatedRect(x, y, width, height, rotate, color) {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    ctx.rotate((Math.PI/180)*rotate);
+
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
